7f550e3d44199456ce3873905e2556b725b6da37
IrfanNaikwade <irfannayakvade2230@gmail.com>
2024-09-29 09:41:27 +0530

    Added Mission page...

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Navbar } from "./components/navbar";
 import { HeroSect } from "./components/heroSect";
+import { OurMission } from "./components/ourMission";
 function App() {
   return (
     <>
         <Navbar/>
         <HeroSect/>
+        <OurMission/>
     </>
   )
 }
---
+++ src/components/ourMission.jsx
@@ -0,0 +1,12 @@
+export const OurMission = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center my-40 w-full">
+        <div className="font-bold uppercase text-3xl">Our Mission</div>
+        <div className="subText w-1/2 text-center font-tiro font-medium text-base mt-5">
+        सोलापूर जिल्ह्यातील करमाळा तालुक्यात तीन हजार लोकसंख्या असणारे नागांचा संभाळ करणारे विविध परंपरा त्याचे जतन संवर्धन करत सन वेगवेगळे सण उत्सव उत्साहात साजरा करणार आधुनिक पद्धतीने शेती करून आर्थिक उन्नती साधणाऱ्या या गावाविषयी
+        </div>
+      </div>
+    </>
+  );
+};
