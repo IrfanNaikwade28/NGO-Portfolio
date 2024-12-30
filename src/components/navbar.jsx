0efc9351a2cce775f272983d9b11d72c33e3c8b6
Sandesh-Pol <sandeshpol268@example.com>
2024-12-30 16:42:47 +0530

    Font size updated

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
       <div className="navbar flex justify-between border-b border-[#00000040] px-20 mt-1">
         <div className="flex gap-x-20 items-center mt-1 py-1">
           <div className="logo color-gradient font-bold text-5xl bebas-neue-regular">LOGO</div>
-          <div className="nav-items flex gap-x-10 font-tiro text-xs font-normal">
+          <div className="nav-items flex gap-x-10 font-tiro text-sm font-normal">
             <div className="nav-item cursor-pointer">नागनाथ तीर्थक्षेत्र</div>
             <div className="nav-item cursor-pointer">उत्सव</div>
             <div className="nav-item cursor-pointer">संस्था मंडळे</div>
